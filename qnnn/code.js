edc604fd7bce6099180916f6a3e218b692f08a26
Tin Dinh <tinutmap@gmail.com>
2023-02-16 11:09:15 -0500

    qnnn template

---
+++ qnnn/code.js
@@ -0,0 +1,6 @@
+
+
+// Below is for internal testing
+module.exports = {
+    f: testFunction
+} 
---
+++ qnnn/code.test.js
@@ -0,0 +1,9 @@
+// // import f from code
+// const { f } = require('./code')
+
+test('test 1', () => {
+    const input = undefined
+    const re = undefined
+    // expect(f(input)).toStrictEqual(re)
+    // expect(input).toStrictEqual(re)
+})
