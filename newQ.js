28651086a3780a837ee4252f39afb27351e922bb
Tin Dinh <tinutmap@gmail.com>
2023-02-16 21:30:06 -0500

    newQ script

---
+++ newQ.js
@@ -0,0 +1,27 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+import promptSync from 'prompt-sync'; // credit: https://stackoverflow.com/a/65852551/16236333
+const prompt = promptSync();
+
+async function copyDir (src, dest) {
+    await fs.mkdir(dest, { recursive: true });
+    let entries = await fs.readdir(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+        let srcPath = join(src, entry.name);
+        let destPath = join(dest, entry.name);
+
+        entry.isDirectory() ?
+            await copyDir(srcPath, destPath) :
+            await fs.copyFile(srcPath, destPath);
+    }
+}
+
+try {
+    const q = prompt('Question number: '),
+        qn = q.match(/\d+/)[0],
+        templateFolderName = 'qnnn'
+    if (qn) copyDir('./' + templateFolderName, './q' + qn)
+} catch (e) {
+    console.log(e)
+}
