856cb75d33ba461545adc7bd0cb4a1ebf3277ceb
Tin Dinh <tinutmap@gmail.com>
2023-02-19 15:27:24 -0500

    q142

--- q142/code.js
+++ q142/code.js
@@ -1,14 +1,51 @@
-// Question link: 
+// Question link: https://leetcode.com/problems/linked-list-cycle-ii/description/?envType=study-plan&id=level-1
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
 
-// Solution 1:
-// Solution link
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  // Solution 1: using Set. not optimal with O(1) memory.
+  // Solution link
+  while (head) {
+    if (!d.has(head)) {
+      d.add(head);
+      head = head.next;
+    } else return head;
+  }
+  return null;
 
-// Forum Solution:
-// Solution link
-// }
+  //   while (head) {
+  //     if (head.next !== head) {
+  //       let cur = head;
+  //       head = head.next;
+  //       cur.next = cur;
+  //     } else {
+  //       return head;
+  //     }
+  //   }
+  //   return null;
+
+  //   let fast = (slow = head);
+  //   while (fast.next) {
+  //     fast = fast.next.next;
+  //     if (fast === slow) return fast.next;
+  //     slow = slow.next;
+  //   }
+  //   return null;
+  // Forum Solution: see turtle and hare pointers explanation.
+  // Solution link https://leetcode.com/problems/linked-list-cycle-ii/solutions/1701128/c-java-python-slow-and-fast-image-explanation-beginner-friendly/?envType=study-plan&id=level-1&orderBy=most_votes
+};
 
 // Below is for internal testing
 module.exports = {
-    f: testFunction
-} 
+  f: detectCycle,
+};
--- q142/code.test.js
+++ q142/code.test.js
@@ -1,8 +1,15 @@
-// // import f from code
-// const { f } = require('./code')
+// import f from code
+const { f } = require("./code");
+const {
+  createSinglyLinkedList,
+  linkListToArray,
+} = require("../linkedList/base");
 
-test('test 1', () => {
-    const input = undefined
-    const re = undefined
-    // expect(f(input)).toStrictEqual(re)
-})
+test("test 1", () => {
+  const head = [3, 2, 0, -4];
+  re = 1;
+  const headList = createSinglyLinkedList(head),
+    fToArray = linkListToArray(f(headList));
+
+  expect(fToArray).toStrictEqual(re);
+});
