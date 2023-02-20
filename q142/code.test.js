49970b7cf530af209eb2d9a6b3023f0aad402f7e
Tin Dinh <tinutmap@gmail.com>
2023-02-20 12:49:41 -0500

    q142 dummy test

--- q142/code.test.js
+++ q142/code.test.js
@@ -6,7 +6,7 @@ const {
 } = require("../linkedList/base");
 
 test("test 1", () => {
-  // This test is not working
+  // This test is only a dummy one as I was lazy to create a loop for test case
   const head = [3, 2, 0, -4];
   const headList = createSinglyLinkedList(head),
     fToArray = linkListToArray(f(headList)),
